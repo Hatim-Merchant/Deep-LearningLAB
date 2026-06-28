5eaab943ac595b54fe200190c709217d513ad19d
Lisa <bilin.qi@gmail.com>
2026-06-28 15:19:05 +0200

    Add run script for continual learning experiment with freezing

---
+++ freeze.sh
@@ -0,0 +1,1 @@
+python ./continual_learning.py --pretrained-dir "./experiments/vit_pretrained_T1and2_cifar10_60epochs" --exp-name "run1" --epochs-per-task 30 --batch-size 128 --lr 0.001 --save-dir "./experiments" --freeze-heads --freeze-ratio 0.2
